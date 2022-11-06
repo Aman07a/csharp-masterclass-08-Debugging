c97f0d686a3f43d2fc4a6fabec26544877f33a79
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-06 21:59:58 +0100

    Debugging Basics

--- 08 - Debugging/Program.cs
+++ 08 - Debugging/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08___Debugging
 {
@@ -6,7 +7,39 @@ namespace _08___Debugging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
+            var partyFriends = GetPartyFriends(friends, 4);
+
+            foreach (var name in partyFriends)
+                Console.WriteLine(name);
+
+            Console.Read();
+        }
+
+        public static List<string> GetPartyFriends(List<string> list, int count)
+        {
+            var partyFriends = new List<string>();
+
+            while (partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(list);
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+            return partyFriends;
+        }
+
+        private static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length > shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+            return shortestName;
         }
     }
 }
