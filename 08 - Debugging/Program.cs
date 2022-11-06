fd830db088a867bba577c177e2e36dcabd3cb838
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-06 22:20:48 +0100

    Debugging Call Stack, Throwing Errors and defensive programming

--- 08 - Debugging/Program.cs
+++ 08 - Debugging/Program.cs
@@ -8,6 +8,7 @@ namespace _08___Debugging
         static void Main(string[] args)
         {
             var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
+            // var friends = new List<string>();
             var partyFriends = GetPartyFriends(friends, 4);
 
             // foreach (var name in friends)
@@ -21,6 +22,12 @@ namespace _08___Debugging
 
         public static List<string> GetPartyFriends(List<string> list, int count)
         {
+            if (list == null)
+                throw new ArgumentNullException("List", "The list is empty");
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count cannot be greater then elements in the list or lower then 0");
+
             var buffer = new List<string>(list);
             var partyFriends = new List<string>();
 
