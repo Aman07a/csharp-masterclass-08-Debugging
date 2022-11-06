e0babeafa51a694d021e818df06a7a31929200dd
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-06 22:11:32 +0100

    Debugging, Creating Copies of Lists and solving some bugs

--- 08 - Debugging/Program.cs
+++ 08 - Debugging/Program.cs
@@ -10,6 +10,9 @@ namespace _08___Debugging
             var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
             var partyFriends = GetPartyFriends(friends, 4);
 
+            // foreach (var name in friends)
+            //     Console.WriteLine(name);
+
             foreach (var name in partyFriends)
                 Console.WriteLine(name);
 
@@ -18,23 +21,31 @@ namespace _08___Debugging
 
         public static List<string> GetPartyFriends(List<string> list, int count)
         {
+            var buffer = new List<string>(list);
             var partyFriends = new List<string>();
 
             while (partyFriends.Count < count)
             {
-                var currentFriend = GetPartyFriend(list);
+                // var currentFriend = GetPartyFriend(list);
+                var currentFriend = GetPartyFriend(buffer);
                 partyFriends.Add(currentFriend);
-                list.Remove(currentFriend);
+                // list.Remove(currentFriend);
+                buffer.Remove(currentFriend);
             }
             return partyFriends;
         }
 
+        /// <summary>
+        /// This is the logic to figure out who is the partyFriend
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         private static string GetPartyFriend(List<string> list)
         {
             string shortestName = list[0];
             for (var i = 0; i < list.Count; i++)
             {
-                if (list[i].Length > shortestName.Length)
+                if (list[i].Length < shortestName.Length)
                 {
                     shortestName = list[i];
                 }
